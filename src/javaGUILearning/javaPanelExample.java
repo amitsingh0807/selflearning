89acaa65b8299c86bf053c9b8527f5cff1446e3f
amitsingh0807 <amit.learningservices@gmail.com>
2018-01-30 14:20:55 -0600

    Amit try3

--- src/javaGUILearning/javaPanelExample.java
+++ src/javaGUILearning/javaPanelExample.java
@@ -34,5 +34,16 @@ class ImagePanel extends JPanel{
 	
 	public ImagePanel(Image img) {
 		this.img = img;
+		Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+		setPreferredSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+		setSize(size);
+		setLayout(null);
+				
+	}
+	
+	public void paintComponent(Graphics g) {
+		g.drawImage(img,0,0,null);
 	}
 }
