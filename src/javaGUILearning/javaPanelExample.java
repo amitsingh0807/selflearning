221f4c76a324d1d7b1ef0e51019a6edace71b6ab
amitsingh0807 <amit.learningservices@gmail.com>
2018-01-30 21:52:35 -0600

    Amit try 4

--- src/javaGUILearning/javaPanelExample.java
+++ src/javaGUILearning/javaPanelExample.java
@@ -7,17 +7,16 @@ import javax.swing.JPanel;
 
 import java.awt.Image;
 import java.awt.Graphics;
+import java.awt.Color;
 import java.awt.Dimension;
-
-
 
 
 public class javaPanelExample {
 	public static void main(String [] args) {
 		ImagePanel panel1 = new ImagePanel(new ImageIcon("C://Users/amitsin/Pictures/javaImage.jpg").getImage());
 		
-		panel1.setName("My Image");
 		JFrame frame1 = new JFrame();
+		frame1.setTitle("What the hell");
 		frame1.getContentPane().add(panel1);
 		frame1.pack();
 		frame1.setVisible(true);
@@ -30,11 +29,13 @@ class ImagePanel extends JPanel{
 	private Image img;
 	public ImagePanel(String img) {
 		this(new ImageIcon(img).getImage());
+		this.setName("My ImagePanel");
+		this.setBackground(Color.BLUE);
 	}
 	
 	public ImagePanel(Image img) {
 		this.img = img;
-		Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+		Dimension size = new Dimension((img.getWidth(null))/2, (img.getHeight(null))/2);
 		setPreferredSize(size);
 		setMinimumSize(size);
 		setMaximumSize(size);
